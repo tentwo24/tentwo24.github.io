3c1bf857da2f2b484dc09616197bf2c9ddef8ba5
TenTwo <472948434@qq.com>
2024-06-03 17:12:06 +0800

    Site updated: 2024-06-03 17:12:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/03/hello-world/"];function toRandomPost(){
+var posts=["2024/06/03/webpack的基本用法/","2024/06/03/webpack的高级应用/","2024/06/03/关于VueRouter/","2024/06/03/关于pinia和vuex的简单应用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
